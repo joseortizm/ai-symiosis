be066e400f5963f6b4c41041e4d8f37d9ae8c955
Dionysis Athinaios <d.athinaios@gmail.com>
2025-09-08 09:53:01 +0100

    Add state.rs preparing for migration of globals

---
+++ src-tauri/src/core/state.rs
@@ -0,0 +1,36 @@
+use crate::config::AppConfig;
+use std::sync::{atomic::AtomicBool, Arc, RwLock};
+
+#[derive(Clone)]
+pub struct AppState {
+    pub config: Arc<RwLock<AppConfig>>,
+    pub was_first_run: Arc<AtomicBool>,
+    pub programmatic_operation_in_progress: Arc<AtomicBool>,
+}
+
+impl AppState {
+    pub fn new(config: AppConfig) -> Self {
+        Self {
+            config: Arc::new(RwLock::new(config)),
+            was_first_run: Arc::new(AtomicBool::new(false)),
+            programmatic_operation_in_progress: Arc::new(AtomicBool::new(false)),
+        }
+    }
+
+    pub fn set_first_run(&self, value: bool) {
+        self.was_first_run
+            .store(value, std::sync::atomic::Ordering::Relaxed);
+    }
+
+    pub fn get_config(&self) -> Arc<RwLock<AppConfig>> {
+        Arc::clone(&self.config)
+    }
+
+    pub fn was_first_run(&self) -> &AtomicBool {
+        &self.was_first_run
+    }
+
+    pub fn programmatic_operation_in_progress(&self) -> &AtomicBool {
+        &self.programmatic_operation_in_progress
+    }
+}
